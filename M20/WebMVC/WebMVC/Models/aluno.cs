023efe5d881c2450422a23c7bac6fee6e1390007
Jose fernandes <zefrancisco2002@gmail.com>
2020-12-14 14:57:43 +0000

    WebMVC

---
+++ M20/WebMVC/WebMVC/Models/aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class aluno
+    {
+        public int Naluno { get; set; }
+
+        public string PriNome { get; set; }
+        public string UltNome { get; set; }
+        public string Morada { get; set; }
+        public Genero Genero { get; set; }
+        public DateTime DataNasc { get; set; }
+        public int AnoEscolaridade { get; set; }
+        public string ImgPath { get; set; }
+    }
+
+    public class Genero
+    {
+
+    }
+}
